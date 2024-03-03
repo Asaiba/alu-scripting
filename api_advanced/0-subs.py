67fcef81f360a8ff92e8c132c93812863b600ff2
Asaiba <a.saiba@alustudent.com>
2024-03-03 18:09:41 +0200

    try

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -10,24 +10,23 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a given subreddit.
     """
-    # Set the Default URL strings
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
-
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
-
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
-                       allow_redirects=False)
-
-    # Checks if the subreddit is invalid
-    if res.status_code in repr([302, 404]):
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If not a valid subreddit, returns 0.
+    """
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
+    response = requests.get(url, headers=headers)
+    
+    # If the response status code is 404, the subreddit is not valid
+    if response.status_code == 404:
         return 0
-
-    # Returns the total subscribers of the subreddit
-    return res.json().get('data').get('subscribers')
+    
+    # If the response status code is 200, the subreddit is valid
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    
+    # If the response status code is neither 404 nor 200, there was an error
+    raise Exception(f'Error {response.status_code}: {response.text}')
