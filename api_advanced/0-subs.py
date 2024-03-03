074e026c78fd0d4fce899ebbebd9cd3dcedf6a54
Asaiba <a.saiba@alustudent.com>
2024-03-03 17:24:50 +0200

    rrr

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -30,12 +30,26 @@ def get_subreddit_subscribers(subreddit):
            raise e
    except requests.exceptions.RequestException as e:
        raise e
-# Testing the function with valid input
-subreddit = "python"
-result = get_subreddit_subscribers(subreddit)
-print(result) # Should print the number of subscribers for r/python
 
-# Testing the function with invalid input
-subreddit = "invalid_subreddit"
-result = get_subreddit_subscribers(subreddit)
-print(result) # Should print 0
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers
+    for a given subreddit.
+    """
+    # Set the Default URL strings
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
+                                                       subreddit=subreddit)
+
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=user_agent,
+                       allow_redirects=False)
+
+    # Checks if the subreddit is invalid
+    if res.status_code in [302, 404]:
+        return 0
+
+    # Returns the total subscribers of the subreddit
+    return res.json().get('data').get('subscribers')
