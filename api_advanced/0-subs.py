dafe716f3fb703755a573f1359a0fad87ea06f8d
Asaiba <a.saiba@alustudent.com>
2024-03-03 17:23:53 +0200

    ff

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,26 +8,34 @@ the function should return 0.
 """
 import requests
 
+def get_subreddit_subscribers(subreddit):
+   """Queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.
+   If an invalid subreddit is given, the function should return 0.
 
-def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a given subreddit.
-    """
-    # Set the Default URL strings
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
+   Parameters:
+   subreddit (str): The name of the subreddit.
 
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
+   Returns:
+   int: The number of subscribers in the given subreddit. If the subreddit is invalid, returns 0.
+   """
+   try:
+       response = requests.get(f"https://api.reddit.com/r/{subreddit}/about", headers={"User-Agent": "My User Agent 1.0"})
+       response.raise_for_status()
+       data = response.json()
+       return data["data"]["subscribers"]
+   except requests.exceptions.HTTPError as e:
+       if e.response.status_code == 404:
+           return 0
+       else:
+           raise e
+   except requests.exceptions.RequestException as e:
+       raise e
+# Testing the function with valid input
+subreddit = "python"
+result = get_subreddit_subscribers(subreddit)
+print(result) # Should print the number of subscribers for r/python
 
-    # Get the Response of the Reddit API
-    res = requests.get(api_uri, headers=user_agent,
-                       allow_redirects=False)
-
-    # Checks if the subreddit is invalid
-    if res.status_code in [302, 404]:
-        return 0
-
-    # Returns the total subscribers of the subreddit
-    return res.json().get('data').get('subscribers')
+# Testing the function with invalid input
+subreddit = "invalid_subreddit"
+result = get_subreddit_subscribers(subreddit)
+print(result) # Should print 0
