dca90db8230d37cb6638deea461b8dc5d17446cd
Asaiba <a.saiba@alustudent.com>
2024-03-03 18:28:48 +0200

    w

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -10,23 +10,25 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers
+    for a given subreddit.
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If not a valid subreddit, returns 0.
-    """
+    # Set the Default URL strings
     headers = {'User-Agent': 'my-app/0.0.1'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    response = requests.get(url, headers=headers)
-    
-    # If the response status code is 404, the subreddit is not valid
-    if response.status_code == 404:
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
+                                                       subreddit=subreddit)
+
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=headers,
+                       allow_redirects=False)
+
+    # Checks if the subreddit is invalid
+    if res.status_code in repr([302, 404]):
         return 0
-    
-    # If the response status code is 200, the subreddit is valid
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    
-    # If the response status code is neither 404 nor 200, there was an error
-    raise Exception(f'Error {response.status_code}: {response.text}')
+
+    # Returns the total subscribers of the subreddit
+    return res.json().get('data').get('subscribers')
